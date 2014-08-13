6b820d01d975b23a756f7dc16a4b28cff1f3f088
0x5A4D <a13.sdps@g.chuo-u.ac.jp>
2014-08-13 19:22:03 +0900

    fix

--- js/geolocation.js
+++ js/geolocation.js
@@ -1,8 +1,8 @@
-$(document).ready(function() {
+document.addEventListener("DOMContentLoaded", function() {
     
     var watchID = navigator.geolocation.watchPosition( function(position) {
         var str = "latitude: "+position.coords.latitude+", longitude: "+position.coords.longitude;
-        $("#geolocation").text(str);
+        document.getElementById("geolocation").textContent = str;
     });
     
 });

d9e68f69ff3569f813f3af072a0f003850fb55b8
0x5A4D <a13.sdps@g.chuo-u.ac.jp>
2014-08-23 08:47:40 +0900

    add Camera API Demo

--- js/camera.js
+++ js/camera.js
@@ -1,28 +1,73 @@
-$(document).ready(function() {
+document.addEventListener("DOMContentLoaded", function() {
+
+    var CameraManager = window.navigator.mozCamera || window.navigator.mozCameras;
+
     var options = {
-        mode: 'picture',
-        recorderProfile: 'jpg',
-        previewSize: {
-            width: 352,
-            height: 288
-        }
+      mode: 'picture',
+      recorderProfile: 'jpg',
+      previewSize: {
+        width: 320,
+        height: 240
+      }
     };
+    
+    var camera = null;
+    var cameraType = CameraManager.getListOfCameras()[0];
+    var idx = 0;
 
-    $('#info').append("test1<br>");
-    var cameras = navigator.mozCameras.getListOfCameras();
-    for(var elm in navigator.mozCameras) {
-        $('#info').append(navigator.mozCameras[elm] + "<br>");
-    }
-
-    function onSuccess(camera) {
-        $('#info').append('Success<br>');
+    function onSuccess(CameraControl) {
+        camera = CameraControl;
+        console.log('success: ', camera);
+        var video = document.getElementById('video');
+        var rot   = (cameraType === 'back') ? 90 : -90;
+        video.mozSrcObject = camera;
+        video.style.transform = 'rotate(' + rot + 'deg)';
+        video.play();
+        
+        // Effect
+        var effectsBtn = document.getElementById('effects');
+        var effects = camera.capabilities.effects;
+        if(effects < 1){
+            effectsBtn.setAttribute('disabled', 'disabled');
+        }else{
+            //console.log(effects);
+            effectsBtn.addEventListener('click', function(){
+                idx = (idx === effects.length) ? 0 : ++idx; 
+                camera.effect = effects[idx];
+            });
+        }
+        
     };
 
     function onError(error) {
-        $('#info').append('エラーが発生しました。再読み込みしてください。');
+        console.warn(error);
     };
+    
+    function releaseCamera(){
+        if(camera){
+            camera.release();
+        }
+    }
 
-    $('#info').append("test2<br>");
-    navigator.mozCameras.getCameras(cameras[0], options, onSuccess, onError);
-    $('#info').append("test3<br>");
+    function getCamera(){
+        releaseCamera();
+        CameraManager.getCamera(cameraType, options, onSuccess, onError);   
+    }
+    
+    // ボタン
+    var start = document.getElementById('start');
+    var end = document.getElementById('end');
+    start.addEventListener('click', function(){
+        getCamera();
+    });
+    end.addEventListener('click', function(){
+        releaseCamera();
+    });
+    
+    // カメラ開始
+    getCamera();
+
+    // 終了時に解放
+    window.addEventListener('unload', releaseCamera);
+    
 });
